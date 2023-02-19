605ecc6fe1585dcf05cc7edc49b1927d5dbea524
44578287 <g9964957@gmail.com>
2023-02-19 22:37:08 +0800

    添加项目文件。

---
+++ 图像压缩/MODE/ImageCompression.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using ImageMagick;
+
+namespace 图像压缩.MODE
+{
+    /// <summary>
+    /// 图像压缩
+    /// </summary>
+    public class ImageCompression
+    {
+        /// <summary>
+        /// 压缩图像
+        /// </summary>
+        /// <param name="FilePathIn">文件名</param>
+        /// <param name="FilePathOut">输出文件名</param>
+        /// <returns></returns>
+        async public static Task<string> Compression(string FilePathIn, string FilePathOut = null)
+        {
+            if (!File.Exists(FilePathIn))//待处理文件不存在
+            {
+                return null;
+            }
+            if (FilePathOut == null || FilePathOut == "")
+            {
+                FilePathOut = $"{Path.GetDirectoryName(FilePathIn)}/{Path.GetFileNameWithoutExtension(FilePathIn)}.webp";//将文件以同名存至同位置(默认保存成webp)
+            }
+            if (Path.GetExtension(FilePathIn) == Path.GetExtension(FilePathOut))//如果扩展名一样直接返回原文件位置
+            {
+                return FilePathIn;
+            }
+            using (var image = new MagickImage(FilePathIn))
+            {
+                await image.WriteAsync(FilePathOut);
+            }
+            return FilePathOut;
+        }
+    }
+}
---
+++ 图像压缩/主程序.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using 图像压缩.MODE;
+
+namespace 图像压缩
+{
+    internal class 主程序
+    {
+        static void Main(string[] args)
+        {
+            System.Console.OutputEncoding = System.Text.Encoding.UTF8;
+            //System.Console.WriteLine(data["title"]);
+            while (true)
+            {
+                string FilesPath = null;
+                Console.WriteLine("请输入文件夹或文件路径");
+                FilesPath = Console.ReadLine();//输入文件路径
+
+                string Format = null;
+                Console.WriteLine("请输入目标格式");
+                Format = Console.ReadLine();//输入文件路径
+
+
+                if (File.Exists(FilesPath))//文件
+                {
+                    Console.WriteLine("努力压缩中...");
+                    string FilePathOut = ImageCompression.Compression(FilesPath, $"{Path.GetDirectoryName(FilesPath)}/{Path.GetFileNameWithoutExtension(FilesPath)}.{Format}").Result;
+                    Console.WriteLine($"完成!文件路径:{Path.GetFullPath(FilePathOut)}");
+                }
+                else if (Directory.Exists(FilesPath))//目录
+                {
+                    Console.WriteLine("努力压缩中...");
+                    Directory.CreateDirectory($"{FilesPath}-ImageCompression");
+                    string TargetDir = $"{FilesPath}-ImageCompression";//输出位置
+                    List<Task<string>> CompressionQueue = new List<Task<string>>();
+                    Director(FilesPath, CompressionQueue, TargetDir, Format);
+                    //CompressionQueue.W
+                    Task.WaitAll(CompressionQueue.ToArray());
+                    Console.WriteLine($"完成!文件夹路径:{Path.GetFullPath(TargetDir)}");
+                }
+            }
+        }
+        public static void Director(string dir, List<Task<string>> list, string FilesPath, string Format)
+        {
+            DirectoryInfo d = new DirectoryInfo(dir);
+            FileInfo[] files = d.GetFiles();//文件
+            DirectoryInfo[] directs = d.GetDirectories();//文件夹
+            foreach (FileInfo f in files)
+            {
+                list.Add(ImageCompression.Compression(f.FullName, $"{FilesPath}/{Path.GetFileNameWithoutExtension(f.Name)}.{Format}"));
+                Console.WriteLine(f.FullName + " 开始压缩");
+                //list.Add(f.Name);//添加文件名到列表中  
+            }
+            //获取子文件夹内的文件列表，递归遍历  
+            foreach (DirectoryInfo dd in directs)
+            {
+                Director(dd.FullName, list, FilesPath, Format);
+            }
+        }
+    }
+}
